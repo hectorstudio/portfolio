eb2d91dc44c6e6e63e90c0d55f07fb5ddad90332
Starstrider <dev.topking0811@gmail.com>
2020-05-13 10:19:37 +0800

    set heroku postbuild script

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
 		REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN:
 			process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
   },
-  defaultPathMap: '/blog',
-	exportPathMap: function(defaultPathMap){
-    const path = {
-			"/": { 
-				page: "/" 
-			}
-		};
-		return path;
-  },
 };
 
 module.exports = withOffline(withCSS(nextConfig));
